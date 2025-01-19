91cd46c1463ffe919f414c053992d4b3f48d6976
julioSilvaDias <juliocesar.silvadi@elorrieta-errekamari>
2025-01-20 00:03:50 +0100

    .

--- ElorServer/src/main/java/com/elor/server/elorServer/socketIO/SocketIOModule.java
+++ ElorServer/src/main/java/com/elor/server/elorServer/socketIO/SocketIOModule.java
@@ -18,29 +18,43 @@ import bbdd.pojos.Usuario;
 public class SocketIOModule {
 
 	private SocketIOServer server = null;
-	
+
 	public SocketIOModule(SocketIOServer server) {
 		super();
 		this.server = server;
-		
+
 		server.addConnectListener(OnConnect());
 		server.addDisconnectListener(OnDisconnect());
-		//Custom Events
+
 		server.addEventListener(Events.ON_LOGIN.value, MessageInput.class, this.login());
 		server.addEventListener(Events.ON_GET_ALL.value, MessageInput.class, this.getAll());
 		server.addEventListener(Events.ON_LOGOUT.value, MessageInput.class, this.logout());
 	}
 
+	private ConnectListener OnConnect() {
+		return (client -> {
+			client.joinRoom("default-room");
+			System.out.println("New connection, Client: " + client.getRemoteAddress());
+		});
+	}
+
+	private DisconnectListener OnDisconnect() {
+		return (client -> {
+			client.leaveRoom("default-room");
+			System.out.println(client.getRemoteAddress() + " disconected from server");
+		});
+	}
+
 	private DataListener<MessageInput> getAll() {
 		return ((client, data, ackSender) -> {
 			System.out.println("Client from " + client.getRemoteAddress() + " wants to getAll");
-			
+
 			List<Usuario> usuarios = new ArrayList<Usuario>();
-			//añadir nuevo usuario
+			// añadir nuevo usuario
 			usuarios.add(new Usuario());
-			
+
 			String answerMessage = new Gson().toJson(usuarios);
-			
+
 			MessageOutput messageOutput = new MessageOutput(answerMessage);
 			client.sendEvent(Events.ON_GET_ALL_ANSWER.value, messageOutput);
 		});
@@ -49,13 +63,13 @@ public class SocketIOModule {
 	private DataListener<MessageInput> logout() {
 		return ((client, data, ackSender) -> {
 			System.out.println("Client form " + client.getRemoteAddress() + " wants to logout");
-			
+
 			String message = data.getMessage();
-			
+
 			Gson gson = new Gson();
 			JsonObject jsonObject = gson.fromJson(message, JsonObject.class);
 			String username = jsonObject.get("login").getAsString();
-			
+
 			System.out.println(username + " loged out");
 		});
 	}
@@ -64,22 +78,22 @@ public class SocketIOModule {
 		// TODO Auto-generated method stub
 		return ((client, data, ackSender) -> {
 			System.out.println("Client from " + client.getRemoteAddress() + "wants to login");
-			
+
 			String message = data.getMessage();
-			
+
 			Gson gson = new Gson();
 			JsonObject jsonObject = gson.fromJson(message, JsonObject.class);
 			String username = jsonObject.get("login").getAsString();
-			
+
 			Usuario usuario = new Usuario();
-			
+
 			String answerMessage = gson.toJson(usuario);
-			
+
 			MessageOutput messageOutput = new MessageOutput(answerMessage);
 			client.sendEvent(Events.ON_LOGIN_ANSWER.value, messageOutput);
 		});
 	}
-	
+
 	public void start() {
 		server.start();
 		System.out.println("Server started...");
@@ -90,33 +104,4 @@ public class SocketIOModule {
 		System.out.println("Server stopped");
 	}
 
-	private ConnectListener OnConnect() {
-		return (client -> {
-			client.joinRoom("default-room");
-			System.out.println("New connection, Client: " + client.getRemoteAddress());
-		});
-	}
-	
-	private DisconnectListener OnDisconnect() {
-		return (client -> {
-			client.leaveRoom("default-room");
-			System.out.println(client.getRemoteAddress() + " disconected from server");
-		});
-	}
-	
-	private void addConnectListener(ConnectListener onConnect) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void addDisconnectListener(DisconnectListener onDisconnect) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void addEventListener() {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }

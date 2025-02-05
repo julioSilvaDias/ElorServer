7f252d8cf9ef20bf38a6639fbcb8786b5ffc49af
julioSilvaDias <juliocesar.silvadi@elorrieta-errekamari>
2025-02-05 14:01:27 +0100

    .

--- ElorServer/src/main/java/bbdd/utils/Utils.java
+++ ElorServer/src/main/java/bbdd/utils/Utils.java
@@ -1,7 +1,7 @@
 package bbdd.utils;
 
 public class Utils {
-	public static final String URL = "jdbc:mysql://10.5.104.50:3306/ElorBase";
+	public static final String URL = "jdbc:mysql://192.168.0.21:3306/ElorBase";
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	public static final String USER = "root";
 	public static final String PASS = "Elorrieta00";
